6cc0bd509a3392a7e4f870771aec9fe9c267c5df
Yu-Zhang666 <47916225+Yu-Zhang666@users.noreply.github.com>
2019-12-27 12:12:26 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,3 +4,10 @@ for i in range(1,10):
     for j in range(i,10):
         print("%d*%d=%2d" % (i,j,i*j),end=" ")
     print("")
+#第二种
+for i in range(1,10):
+    for k in range(1,i):
+        print(end="        ")
+    for j in range(i,10):
+        print("{0:1}×{1:1}={2:<2}".format(i,j,i*j),end=' ')
+    print("")
